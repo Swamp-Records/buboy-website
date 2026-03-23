f02d139409fd5390def6549e12dd604f5cba1aa0
IshanBhatBhardwaj <ishanbhatbhardwaj@icloud.com>
2026-03-23 13:41:33 -0400

    project set up

---
+++ app/pressKit/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PressKit () {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            This is the Press Kit Page! 
+        </div>
+    )
+}
---
+++ app/shows/page.tsx
@@ -0,0 +1,7 @@
+export default function Shows () {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            This is the Shows Page! 
+        </div>
+    )
+}
